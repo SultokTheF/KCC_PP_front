fff15a53e96db50286a588e719a86f6a0d40ee54
SultokTheF <sultok.003@gmail.com>
2024-09-03 15:38:11 +0500

    feat: export function implemented

--- src/modules/admin/components/Reports/Disbalance/DisbalancsTable.jsx
+++ src/modules/admin/components/Reports/Disbalance/DisbalancsTable.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import * as XLSX from 'xlsx';
 
 const DisbalanceTable = ({ formData, subjectsList, daysList, hoursList, setFormData }) => {
   const [plansData, setPlansData] = useState([]);
@@ -82,55 +83,91 @@ const DisbalanceTable = ({ formData, subjectsList, daysList, hoursList, setFormD
     '23 - 00'
   ];
 
+  const exportToExcel = () => {
+    // Prepare the data for the XLSX file
+    const data = formData.dateArray.flatMap((date, dateIndex) =>
+      plansData[dateIndex].map((hour, hourIndex) => ({
+        Date: date,
+        Time: timeIntervals[hourIndex],
+        Plan: hour.plan,
+        ...(subject?.subject_type === "ЭПО" && { "Plan Generation": hour.planGen }),
+        Fact: hour.fact,
+        ...(subject?.subject_type === "ЭПО" && { "Fact Generation": hour.factGen }),
+        "BE Up": hour.BE_Up,
+        "BE Down": hour.BE_Down,
+        "OD Up": hour.OD_Up,
+        "OD Down": hour.OD_Down,
+      }))
+    );
+
+    // Create a worksheet from the data
+    const worksheet = XLSX.utils.json_to_sheet(data);
+
+    // Create a new workbook and append the worksheet
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Disbalance");
+
+    // Write the workbook and trigger the download
+    XLSX.writeFile(workbook, 'Disbalance.xlsx');
+  };
+
   if (!formData.dateArray || !plansData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <table className="table-auto h-3 text-xs text-center w-full border-collapse border border-gray-400">
-      <thead>
-        <tr>
-          <th className="border border-gray-300 px-4">Дата</th>
-          <th className="border border-gray-300 px-4">Время</th>
-          <th className="border border-gray-300 px-4">План</th>
-          {subject?.subject_type === "ЭПО" && (
-            <th className="border border-gray-300 px-4">План Генерации</th>
-          )}
-          <th className="border border-gray-300 px-4">Факт</th>
-          {subject?.subject_type === "ЭПО" && (
-            <th className="border border-gray-300 px-4">Факт Генерации</th>
-          )}
-          <th className="border border-gray-300 px-4">BE ⬆️ (20%)</th>
-          <th className="border border-gray-300 px-4">BE ⬇️ (20%)</th>
-          <th className="border border-gray-300 px-4">OD ⬆️ (20%)</th>
-          <th className="border border-gray-300 px-4">OD ⬇️ (20%)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {formData.dateArray.map((date, dateIndex) => (
-          (plansData[dateIndex] || []).map((hour, hourIndex) => (
-            <tr key={`${date}-${hourIndex}`}>
-              {hourIndex === 0 && (
-                <td className="border border-gray-300 px-4" rowSpan={24}>{date}</td>
-              )}
-              <td className="border border-gray-300 px-4">{timeIntervals[hourIndex]}</td>
-              <td className="border border-gray-300 px-4">{hour.plan}</td>
-              {subject?.subject_type === "ЭПО" && (
-                <td className="border border-gray-300 px-4">{hour.planGen}</td>
-              )}
-              <td className="border border-gray-300 px-4">{hour.fact}</td>
-              {subject?.subject_type === "ЭПО" && (
-                <td className="border border-gray-300 px-4">{hour.factGen}</td>
-              )}
-              <td className={`border border-gray-300 ${hour.BE_Up === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.BE_Up}</td>
-              <td className={`border border-gray-300 ${hour.BE_Down === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.BE_Down}</td>
-              <td className={`border border-gray-300 ${hour.OD_Up === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.OD_Up}</td>
-              <td className={`border border-gray-300 ${hour.OD_Down === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.OD_Down}</td>
-            </tr>
-          ))
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <button
+        onClick={exportToExcel}
+        className="bg-green-500 text-white font-medium py-2 px-4 rounded hover:bg-green-700 mb-4"
+      >
+        Экспорт в Excel
+      </button>
+      <table className="table-auto h-3 text-xs text-center w-full border-collapse border border-gray-400">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 px-4">Дата</th>
+            <th className="border border-gray-300 px-4">Время</th>
+            <th className="border border-gray-300 px-4">План</th>
+            {subject?.subject_type === "ЭПО" && (
+              <th className="border border-gray-300 px-4">План Генерации</th>
+            )}
+            <th className="border border-gray-300 px-4">Факт</th>
+            {subject?.subject_type === "ЭПО" && (
+              <th className="border border-gray-300 px-4">Факт Генерации</th>
+            )}
+            <th className="border border-gray-300 px-4">BE ⬆️ (20%)</th>
+            <th className="border border-gray-300 px-4">BE ⬇️ (20%)</th>
+            <th className="border border-gray-300 px-4">OD ⬆️ (20%)</th>
+            <th className="border border-gray-300 px-4">OD ⬇️ (20%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formData.dateArray.map((date, dateIndex) => (
+            (plansData[dateIndex] || []).map((hour, hourIndex) => (
+              <tr key={`${date}-${hourIndex}`}>
+                {hourIndex === 0 && (
+                  <td className="border border-gray-300 px-4" rowSpan={24}>{date}</td>
+                )}
+                <td className="border border-gray-300 px-4">{timeIntervals[hourIndex]}</td>
+                <td className="border border-gray-300 px-4">{hour.plan}</td>
+                {subject?.subject_type === "ЭПО" && (
+                  <td className="border border-gray-300 px-4">{hour.planGen}</td>
+                )}
+                <td className="border border-gray-300 px-4">{hour.fact}</td>
+                {subject?.subject_type === "ЭПО" && (
+                  <td className="border border-gray-300 px-4">{hour.factGen}</td>
+                )}
+                <td className={`border border-gray-300 ${hour.BE_Up === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.BE_Up}</td>
+                <td className={`border border-gray-300 ${hour.BE_Down === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.BE_Down}</td>
+                <td className={`border border-gray-300 ${hour.OD_Up === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.OD_Up}</td>
+                <td className={`border border-gray-300 ${hour.OD_Down === 0 ? "" : "bg-blue-500 text-white"} px-4`}>{hour.OD_Down}</td>
+              </tr>
+            ))
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
